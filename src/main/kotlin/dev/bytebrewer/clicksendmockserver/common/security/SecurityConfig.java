f04fcf0d130e517086c715620444a8b602fd9608
AleNegrini <alessandro2.negrini@gmail.com>
2024-05-02 22:20:29 +0200

    feat: add and configure spring security

---
+++ src/main/kotlin/dev/bytebrewer/clicksendmockserver/common/security/SecurityConfig.java
@@ -0,0 +1,43 @@
+package dev.bytebrewer.clicksendmockserver.common.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
+        return security.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize->authorize.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService()
+    {
+        UserDetails user = User.builder()
+                .username("clicksend")
+                .password(passwordEncoder().encode("clicksend"))
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+}
